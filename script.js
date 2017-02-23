f6317cf8703d44c8754ca388033071234b449830
hativatmon <morand@HQ.team.co.il>
2017-02-23 16:35:19 +0200

    add open closet

--- script.js
+++ script.js
@@ -12,27 +12,35 @@
 	}
 	
 	// Validating Empty Field
-function check_empty() {
-if (document.getElementById('y').value == "" || document.getElementById('r').value == "" 
-|| document.getElementById('b').value == "" || document.getElementById('g').value == "") {
-alert("יש למלא את כל השדות!");
-} else {
+	function check_empty() {
+	if (document.getElementById('y').value == "" || document.getElementById('r').value == "" 
+	|| document.getElementById('b').value == "" || document.getElementById('g').value == "") {
+	alert("יש למלא את כל השדות!");
+	} else {
+		
+		if (document.getElementById('y').value == "3" & document.getElementById('r').value == "2" 
+	& document.getElementById('b').value == "3" & document.getElementById('g').value == "1"){
+		alert("Code OK");
+		div_hide("ClosetCode");
+		div_show("truiOpen");
+		
+	}
+	else {
+		alert("code BAD");
+	}
+	}
+	}
+	//Function To Display Popup
+	function div_show(ObjId) {
+		document.getElementById(ObjId).style.display = "block";
+	}
+	//Function to Hide Popup
+	function div_hide(ObjId){
+	document.getElementById(ObjId).style.display = "none";
+	}
 	
-	if (document.getElementById('y').value == "3" & document.getElementById('r').value == "2" 
-& document.getElementById('b').value == "3" & document.getElementById('g').value == "1"){
-	alert("Code OK");
-	document.getElementById('form').submit();
-}
-else {
-	alert("code BAD");
-}
-}
-}
-//Function To Display Popup
-function div_show() {
-document.getElementById('abc').style.display = "block";
-}
-//Function to Hide Popup
-function div_hide(){
-document.getElementById('abc').style.display = "none";
-}
+	function thrug_options()
+	{
+		alert("האופציות לאירוע חטיבה הן:\n1. יום ספורט\n2. שייט אגמים\n3. ערב חטיבה\n4. טיסה לחול\n5. טיול חד יומי בארץ\n6. רכיבה אתגרית\n7. נופש בארץ\n8. פעילות אתגרית\n");
+		div_hide("abc");
+	}
